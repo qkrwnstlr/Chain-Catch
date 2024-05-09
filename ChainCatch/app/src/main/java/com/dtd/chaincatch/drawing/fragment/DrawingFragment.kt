db23d36560d71ddce5d14b424fe62e4549b3f628
parkjunsik <akfldkskgorn@gmail.com>
2024-05-09 13:12:28 +0900

    CHC - 11 | feat : firebase realtime database test

--- ChainCatch/app/src/main/java/com/dtd/chaincatch/drawing/fragment/DrawingFragment.kt
+++ ChainCatch/app/src/main/java/com/dtd/chaincatch/drawing/fragment/DrawingFragment.kt
@@ -1,15 +1,30 @@
 package com.dtd.chaincatch.drawing.fragment
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import java.util.Base64
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.dtd.chaincatch.databinding.FragmentDrawingBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.raed.rasmview.brushtool.data.Brush
 import com.raed.rasmview.brushtool.data.BrushesRepository
 import com.raed.rasmview.state.RasmState
+import java.io.ByteArrayOutputStream
+
+
+private const val TAG = "DrawingFragment"
 
 class DrawingFragment : Fragment() {
 
@@ -17,9 +32,33 @@ class DrawingFragment : Fragment() {
   private val binding: FragmentDrawingBinding
     get() = _binding!!
 
+  private lateinit var drawingDB: DatabaseReference
+  private lateinit var drawingRef: DatabaseReference
+
+  private fun initFirebaseDatabase() {
+    drawingDB = Firebase.database.getReference("message")
+    val drawingKey = drawingDB.push().key
+    drawingRef = drawingDB.child(drawingKey!!)
+
+    drawingRef.addValueEventListener(object : ValueEventListener {
+      override fun onDataChange(snapshot: DataSnapshot) {
+        Log.d(TAG, "onDataChange: ")
+        if (drawingKey != snapshot.key) return
+        val data = snapshot.getValue(String::class.java) ?: return
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+          val bitmap = byteArrayToBitmap(Base64.getDecoder().decode(data))
+          binding.imageView.setImageBitmap(bitmap)
+        }
+      }
+
+      override fun onCancelled(error: DatabaseError) {}
+    })
+  }
+
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
+    initFirebaseDatabase()
   }
 
   override fun onCreateView(
@@ -43,6 +82,11 @@ class DrawingFragment : Fragment() {
 
     val rasmState: RasmState = rasmContext.state
 
+    rasmState.addOnStateChangedListener {
+      val drawingBitmap: Bitmap = rasmContext.exportRasm()
+      drawingRef.setValue(drawingBitmap.toBase64())
+    }
+
     binding.undo.setOnClickListener {
       rasmState.undo()
     }
@@ -57,4 +101,22 @@ class DrawingFragment : Fragment() {
     binding.redo.isEnabled = rasmState.canCallRedo()
   }
 
+  fun byteArrayToBitmap(bytearr: ByteArray): Bitmap {
+    return BitmapFactory.decodeByteArray(bytearr, 0, bytearr.size)
+  }
+
+}
+
+fun Bitmap.toByteArray(): ByteArray {
+  val stream = ByteArrayOutputStream()
+  this.compress(Bitmap.CompressFormat.PNG, 100, stream)
+  return stream.toByteArray()
+}
+
+fun Bitmap.toBase64(): String {
+  return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+    String(Base64.getEncoder().encode(this.toByteArray()))
+  } else {
+    ""
+  }
 }
